fc160cf59131805fa0fc7d0713753d373b478295
zchochul <87480906+zchochul@users.noreply.github.com>
2022-03-28 20:42:07 +0200

    dodane zabezpieczenia

--- bin.sh
+++ bin.sh
@@ -2,19 +2,50 @@
 
 #miejsce ze smietnikiem
 SMIETNIK="/home/zuzanka/Desktop/shellscripts/smietnik1"
-echo "Folder smietnik to: $SMIETNIK"
+
 
 if [ -d "$SMIETNIK" ]  # sprawdzamy czy istnieje folder smietnik
   then
         echo "istnieje folder ustalony za smietnik"
   else
-	echo "ustalony folder smietnik nie istnieje, tworze folder smietnik..."
+	echo "Ustalony folder smietnik nie istnieje, tworze folder smietnik..."
 	mkdir $SMIETNIK
+fi
+
+if [ $# = 0 ]
+	then
+	echo "Nie podano argumentow! Sprobuj ponownie"
+	echo "Lub sprawdz opcje --help"
+	exit
 fi
 
 #kompresowanie i przenoszenie do folderu
 for i in "$@"
 do
+	if [ $i -ef $SMIETNIK ] #zabezpieczenie przed usunieciem folderu smietnik
+		then
+		echo "ERROR: No nie mozna usuwac folderu smietnik, nie tak to dziala. Sprobuj ponownie!"
+		exit
+	fi
+	if [ $1 = "--help" ] #pomoc
+		then
+		echo
+		echo "WITAJ W POMOCY DO SKRYPTU KOSZ W BASHU!!"
+		echo "Pliki przenoszone sa do ustalonego folderu smietnik"
+		echo "Pliki do usuniecia dodawaj po spacji wedlug konwencji: ./bin.sh <argument1> <argument2> ..."
+		echo "Folder smietnik to: $SMIETNIK"
+		echo
+		exit
+	fi
+	if [ $1 = "--author" ] #autor
+		then
+		echo
+		echo "Autor: Zuzanna Chochulska"
+		echo "Wydzial Fizyki Politechniki Warszawskiej"
+		echo
+		exit
+	fi
+	
 	if [[ -d $i ]]  # sprawdza czy zmienna jest folderem 
           then
 		#przypadek folderu
